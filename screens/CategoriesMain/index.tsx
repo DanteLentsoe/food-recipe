85b04b663263638c8c1cae6febbdf8dfeffd1091
dantelentsoe <dllentsoe@gmail.com>
2022-02-09 21:08:59 +0200

    feat: dynamic routing

--- screens/CategoriesMain/index.tsx
+++ screens/CategoriesMain/index.tsx
@@ -1,21 +1,50 @@
 import React from "react";
-import { View, Text, Button, StyleSheet, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
+import Theme from "../../constants";
 import { CATEGORIES } from "../../data";
 
-const showGridItems = (itemData: any) => {
-  return (
-    <>
-      <View style={styles.container}>
-        <Text>{itemData.item.title}</Text>
-      </View>
-    </>
-  );
-};
-
 const CatergoriesMain = (props: any) => {
+  const showGridItems = (itemData: any) => {
+    return (
+      <>
+        <TouchableOpacity
+          style={styles.gridContainer}
+          onPress={() => {
+            props.navigation.navigate("CatergoryMealsScreen", {
+              categoryMealId: itemData.item.id,
+            });
+          }}
+        >
+          <View>
+            <Text>{itemData.item.title}</Text>
+          </View>
+        </TouchableOpacity>
+      </>
+    );
+  };
   return (
     <FlatList data={CATEGORIES} numColumns={2} renderItem={showGridItems} />
   );
+};
+
+CatergoriesMain.navigationOptions = {
+  headerTitle: "Meal Categories",
+  headerStyle: {
+    backgroundColor:
+      Platform.OS === "android" ? Theme.colors.primary : Theme.colors.secondary,
+  },
+  headerTintColor: {
+    color:
+      Platform.OS === "android" ? Theme.colors.secondary : Theme.colors.primary,
+  },
 };
 
 export default CatergoriesMain;
@@ -26,5 +55,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  gridContainer: {
+    flex: 1,
+    margin: 15,
+    height: 120,
   },
 });
--- screens/CategoryMeal/index.tsx
+++ screens/CategoryMeal/index.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { CATEGORIES } from "../../data";
+
+interface ICategoriesSelected {
+  id?: string;
+  title?: string;
+  color?: string;
+}
 
 const CatergoryMeal = (props: any) => {
+  const categoryId: string = props.navigation.getParam("categoryMealId");
+
+  // get the unique item in the data
+  const selectedCategory: ICategoriesSelected | undefined = CATEGORIES.find(
+    (item) => item.id === categoryId
+  );
+
   return (
     <View style={styles.container}>
+      <Text>{selectedCategory && selectedCategory.title}</Text>
       <Text>Catergory Meal ................ </Text>
       <Button
         title="Catergory Details"
