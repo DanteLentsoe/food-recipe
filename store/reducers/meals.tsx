0dab029d9af5d0ebc07303d28dc153d40ba4877f
dantelentsoe <dllentsoe@gmail.com>
2022-03-03 10:44:30 +0200

    feat: action dispatching

--- store/reducers/meals.tsx
+++ store/reducers/meals.tsx
@@ -1,4 +1,6 @@
 import { MEALS } from "../../data";
+import Meal from "../../models/mealModal";
+import { TOOGLE_FAV_MEAL } from "../actions";
 
 const intialState = {
   meals: MEALS,
@@ -6,7 +8,29 @@ const intialState = {
   favMeals: [],
 };
 
-const mealReducer = (state = intialState, action: () => void) => {
+const mealReducer = (state = intialState, action: any) => {
+  switch (action.type) {
+    case TOOGLE_FAV_MEAL:
+      const mealFound = state.meals.findIndex(
+        (meal) => meal.id === action.mealId
+      );
+
+      // check if the index is present in Array
+      if (mealFound >= 0) {
+        //copy of array
+        const updateMeals = [...state.favMeals];
+        updateMeals.splice(mealFound, 1);
+        return { ...state, favMeals: updateMeals };
+      } else {
+        const mealAvaliable: ConcatArray<never> | Meal | undefined =
+          state.meals.find((meal) => meal.id === action.mealId);
+
+        // @ts-ignore
+        return { ...state, favMeals: state.favMeals.concat(mealAvaliable) };
+      }
+    default:
+      return state;
+  }
   return state;
 };
 
